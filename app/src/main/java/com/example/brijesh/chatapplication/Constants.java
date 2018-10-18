56166c36202275029e0870655b1c1807fc460a71
brijesh <brij844@gmail.com>
2018-10-18 22:46:52 +0530

    Live url added

--- app/src/main/java/com/example/brijesh/chatapplication/Constants.java
+++ app/src/main/java/com/example/brijesh/chatapplication/Constants.java
@@ -1,5 +1,7 @@
 package com.example.brijesh.chatapplication;
 
 public class Constants {
-   public static String ChatLive = "http://192.168.0.102:3000/";
+ //  public static String ChatLive = "http://192.168.0.102:3000/";
+   public static String ChatLive = "http://18.220.84.252:3000/";
+
 }
